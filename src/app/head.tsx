2a572cfc3ca79d87e8a22b3b94178a3b041d465e
takeourcarsnow <ignasnefas@gmail.com>
2026-04-17 22:37:24 +0300

    Add new favicon image ms-icon-70x70.png to public favicons directory

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,10 +1,14 @@
 export default function Head() {
   return (
     <>
+      <link rel="shortcut icon" href="/favicons/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-icon-180x180.png" />
       <link rel="manifest" href="/manifest.json" />
-      <link rel="icon" href="/icons/icon-192.svg" />
       <meta name="theme-color" content="#0a0a0f" />
-      <link rel="apple-touch-icon" href="/icons/icon-192.svg" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
       <meta name="application-name" content="ReddiTunes" />
     </>
   );
